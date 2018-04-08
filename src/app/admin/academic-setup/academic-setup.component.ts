cf95d08d756ef98bb469e9aced5ccea149c80258
Huzoor Ali <huzoorittech@gmail.com>
2018-04-08 16:22:12 +0530

    Udating Class & Subjects

--- src/app/admin/academic-setup/academic-setup.component.ts
+++ src/app/admin/academic-setup/academic-setup.component.ts
@@ -20,7 +20,9 @@ export class AcademicSetupComponent implements OnInit {
   public subjectList: any[] = new Array();
   public error: any;
   public className: any = '';
+  public class_ID: any = '';
   public subjectName: any = '';
+  public subject_ID: any = '';
   public showUpdateButton: boolean = false;
   constructor(private modalService: BsModalService,
     private dataService: DataService) { }
@@ -89,14 +91,18 @@ export class AcademicSetupComponent implements OnInit {
   }
 
   public editClass(classData, template: TemplateRef<any>): void {
+    console.log('classData', classData)
     this.modalRef = this.modalService.show(template, { ignoreBackdropClick: true });
     this.className = classData.className;
+    this.class_ID = classData._id;
     this.showUpdateButton = true;
   }
 
   public editSubject(subjectData, template: TemplateRef<any>): void {
+    console.log('classData', subjectData)
     this.modalRef = this.modalService.show(template, { ignoreBackdropClick: true });
     this.subjectName = subjectData.subjectName;
+    this.subject_ID = subjectData._id;
     this.showUpdateButton = true;
   }
 
@@ -105,6 +111,7 @@ export class AcademicSetupComponent implements OnInit {
       this.error = '';
       this.classForm.value.instituteUserName = 'inst1-INST';
       this.classForm.value.schoolUserName = 'sch1-SCH';
+      this.classForm.value.fromMode = `create`;
       
       this.dataService.addClass(this.classForm.value)
         .then((resp) => {
@@ -126,7 +133,8 @@ export class AcademicSetupComponent implements OnInit {
     if (this.subjectForm.valid) {
       this.error = '';
       this.subjectForm.value.instituteUserName = 'inst1-INST';
-
+      this.subjectForm.value.fromMode = `create`;
+     
       this.dataService.addSubject(this.subjectForm.value)
         .then((resp) => {
           if (resp.json().success) {
@@ -145,10 +153,51 @@ export class AcademicSetupComponent implements OnInit {
 
   updateClass(classForm): void {
     console.log(classForm.value);
+    if (this.classForm.valid) {
+      this.error = '';
+      this.classForm.value.instituteUserName = 'inst1-INST';
+      this.classForm.value.schoolUserName = 'sch1-SCH';
+      this.classForm.value.fromMode = `update`;
+      this.classForm.value.class_ID = this.class_ID;
+    
+      this.dataService.addClass(this.classForm.value)
+        .then((resp) => {
+          if (resp.json().success) {
+            this.className = new FormControl('',[])
+            this.modalRef.hide();
+            this.getClassesList();
+          } else 
+            this.error = resp.json().message;
+          
+        })
+        .catch((err) => {
+          this.error = err.json().message;
+        });
+    }
   }
 
   updateSubject(subjectForm): void {
     console.log(subjectForm.value);
+    if (this.subjectForm.valid) {
+      this.error = '';
+      this.subjectForm.value.instituteUserName = 'inst1-INST';
+      this.subjectForm.value.fromMode = `update`;
+      this.subjectForm.value.subject_ID = this.subject_ID;
+
+      this.dataService.addSubject(this.subjectForm.value)
+        .then((resp) => {
+          if (resp.json().success) {
+            this.subjectName = new FormControl('',[])
+            this.modalRef.hide();
+            this.getSubjectsList();
+          } else {
+            this.error = resp.json().message;
+          }
+        })
+        .catch((err) => {
+          this.error = err.json().message;
+        });
+    }
   }
 
 }
